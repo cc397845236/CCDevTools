10c582d25449a8ad6568cbaa86ed9b27accefc6c
wangchao.charles <wangchao.charles@bytedance.com>
2019-05-21 11:50:51 +0800

    first commit

---
+++ src/main/java/cc_dev_tools/CcDevToolsApplication.java
@@ -0,0 +1,14 @@
+package cc_dev_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CcDevToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcDevToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc_dev_tools/CcDevToolsApplicationTests.java
@@ -0,0 +1,17 @@
+package cc_dev_tools;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CcDevToolsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
